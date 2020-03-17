edc020f8b7223e87f67c28b6dc6c94b8e4e0fa1d
Samuel Ubellacker <subella@mit.edu>
2020-03-17 16:32:18 -0400

    added style

--- static/work_out.js
+++ static/work_out.js
@@ -3,7 +3,12 @@ var count = time_remaining;
 async function playWorkout(){
   for (var set = current_set; set < work_out.length; set++){
     console.log("Time Remaining: " +time_remaining);
-    document.getElementById("set").innerHTML = work_out[set]["name"];
+    document.getElementById("set").innerHTML = work_out[set]["reps"] + " " +work_out[set]["name"];
+    if (set < work_out.length){
+      var next_set = +set + 1;
+      console.log(next_set);
+      document.getElementById("next").innerHTML = "Up Next: " + work_out[next_set]["reps"] + " " +work_out[next_set]["name"];
+}
     if (set != current_set){
       time_remaining = work_out[set]["intr"];
     }
