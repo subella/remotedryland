04e34f2fed816daea1e7f8c43c7536cc127b1692
Kyri Chen <kyrichen@Kyris-MacBook-Pro.local>
2020-03-23 15:29:44 -0500

    I'm scared I will delete this again

--- functions.js
+++ functions.js
@@ -3,8 +3,10 @@ function parse() {
   document.write(input)
 }
 
+var i = 1
+
 function addExercises() {
-  var number = document.getElementById("exercises").value;
+  var number = document.getElementById("exercises" + i).value;
   var container = document.getElementById("container");
 
   while (container.hasChildNodes()) {
@@ -31,6 +33,33 @@ function addExercises() {
     container.appendChild(para);
     container.appendChild(document.createElement("br"));
     }
+}
+
+function addSet() {
+  i += 1
+  var set_text = document.createTextNode("Set " + i + ": ");
+  var new_set = document.createElement("SELECT");
+  new_set.setAttribute("id", "exercises" + i)
+  new_set.onchange = function() { addExercises() }
+  var set_container = document.getElementById("set_container");
+  var empty = document.createElement("option");
+  empty.text = "# of exercises";
+  new_set.add(empty);
+  var one = document.createElement("option");
+  one.setAttribute("value", "1")
+  one.text = "1";
+  new_set.add(one);
+  var two = document.createElement("option");
+  two.setAttribute("value", "2")
+  two.text = "2";
+  new_set.add(two);
+  var three = document.createElement("option");
+  three.setAttribute("value", "3")
+  three.text = "3";
+  new_set.add(three);
+  set_container.appendChild(set_text);
+  set_container.appendChild(new_set);
+  set_container.appendChild(document.createElement("br"))
 }
 
 function test() {
