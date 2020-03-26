02cb6bbd581f416a0b6b41169294a0ca72ccc305
Samuel Ubellacker <subella@mit.edu>
2020-03-26 10:21:04 -0400

    working with multiple workouts scheduled

---
+++ workouts/workout_list.js
@@ -0,0 +1,17 @@
+var workout_list = [
+  {
+    'date': '2020-03-26T14:19:00Z',
+    'workout': [
+                {'name':'test','reps':'20','intr':'10'},
+                {'name':'test1','reps':'20','intr':'10'}
+               ]
+  },
+
+  {
+    'date': '2020-03-26T14:20:00Z',
+    'workout': [
+                {'name':'test3','reps':'20','intr':'10'},
+                {'name':'test4','reps':'20','intr':'10'}
+               ]
+  }
+]
